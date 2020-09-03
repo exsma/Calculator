dd8391a15167a3f4f26f2acb22f48262122d6f69
Essma Imady <essma@outlook.com>
2020-09-03 18:18:51 -0500

    sum example

---
+++ Sum.py
@@ -0,0 +1,14 @@
+import random
+
+def sum(numbOne, numbTwo):
+    return numbOne + numbTwo
+
+print("")
+numOne = int(input("Please enter number One .. \n"))
+
+print("")
+numTwo = int(input("Please enter number two .. \n"))
+
+results = sum(numOne, numTwo)
+print("Here's the Sum:")
+print(results)
